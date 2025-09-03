10eb65c88a7754f32e622acb8e273f3146b1bb38
psmyth85 <147053283+pwsmyth85@users.noreply.github.com>
2025-09-03 12:01:21 +0100

    Add setup script for installing dependencies

---
+++ setup1.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install -y build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev \
+liblzma-dev git python3-venv
