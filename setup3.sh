f6a1e66027cde59e6790b83e0c8e2d9de781073f
psmyth85 <147053283+pwsmyth85@users.noreply.github.com>
2025-09-03 14:54:57 +0100

    Add setup script for installing dependencies

---
+++ setup3.sh
@@ -0,0 +1,5 @@
+$ sudo apt-get install python python-pip python-dev libffi-dev libssl-dev
+$ sudo apt-get install python-virtualenv python-setuptools
+$ sudo apt-get install libjpeg-dev zlib1g-dev swig
+ sudo apt-get install postgresql libpq-dev
+  sudo apt-get install tcpdump apparmor-utils
