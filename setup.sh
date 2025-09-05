fa631326a60b522f0c3af5f0ee3cfde346d896e3
psmyth85 <147053283+pwsmyth85@users.noreply.github.com>
2025-09-05 15:00:27 +0100

    Reorder Tailscale installation in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-curl -fsSL https://tailscale.com/install.sh | sh
-sudo tailscale up
-
 sudo apt update
 sudo apt install xrdp -y
 sudo systemctl enable --now xrdp
+
+curl -fsSL https://tailscale.com/install.sh | sh
+sudo tailscale up
+
+
